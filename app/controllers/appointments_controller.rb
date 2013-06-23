0122e90aab4a24658b04d6c7cce40a2a8ac6f13d
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-06-23 11:12:14 -0300

    define how multi-cal we be shown

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,16 @@
 class AppointmentsController < ApplicationController
   def index
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @view = params[:view]
   end
 
   def change
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @view = params[:view]
   end
 
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @view = params[:view]
   end
 end
