a55f31eced65af1791b97ca751267ac71f5ece8a
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-06-24 20:20:26 -0300

    more work on day view

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,18 +1,20 @@
 class AppointmentsController < ApplicationController
   before_action :authorize_user
 
-  NAV_TITLE = {day: 'day %Y', week: 'week %Y', weeks2: 'week2 %Y', month: '%B %Y', months2: 'Months 2 %B %Y', year: '%Y', years2: '%Y - %Y 2'}
+  NAV_TITLE = {day: '%A, %B %e %Y', week: 'week %Y', weeks2: 'week2 %Y', month: '%B %Y', months2: 'Months 2 %B %Y', year: '%Y', years2: '%Y - %Y 2'}
 
   def index
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @view = params[:view] ||= 'month'
     @nav_title = @date.strftime(NAV_TITLE[@view.to_sym])
+    @employees = [2, 4, 102]
   end
 
   def change
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
     @view = params[:view] ||= 'month'
     @nav_title = @date.strftime(NAV_TITLE[@view.to_sym])
+    @employees = [2, 4, 102]
   end
 
   private
---
+++ app/helpers/agenda_helper.rb
@@ -0,0 +1,59 @@
+module AgendaHelper
+  def agenda(date = Date.today, employees = [], &block)
+    Agenda.new(self, date, employees, block).table
+  end
+
+  class Agenda < Struct.new(:view, :date, :employees, :callback)
+    FIRST_HOUR = 7
+    LAST_HOUR = 20
+
+    delegate :content_tag, to: :view
+
+    def table
+      content_tag :table, class: 'agenda' do
+        header + hour_rows
+      end
+    end
+
+    def header
+      content_tag :tr do
+        content_tag(:th, '', class: 'corner').html_safe +
+        employees.map { |employee| content_tag :th, 'Employee #' + employee.to_s }.join.html_safe
+      end
+    end
+
+    def hour_rows
+      hours.map do |hour|
+        content_tag :tr do
+          hour_cell(hour).html_safe +
+          employees.map { |employee| employee_cell(employee, hour) }.join.html_safe
+        end
+      end.join.html_safe
+    end
+
+    def hour_cell(hour)
+      content_tag :td, '%02d:00' % hour, class: hour_classes(hour)
+    end
+
+    def employee_cell(employee, hour)
+      content_tag :td, view.capture(employee, hour, &callback), class: employee_classes(hour)
+    end
+
+    def hour_classes(hour)
+      classes = []
+      classes << 'hour'
+      classes.empty? ? nil : classes.join(' ')
+    end
+
+    def employee_classes(hour)
+      classes = []
+      classes.empty? ? nil : classes.join(' ')
+    end
+
+    def hours
+      first = FIRST_HOUR
+      last = LAST_HOUR
+      (first..last).to_a
+    end
+  end
+end
