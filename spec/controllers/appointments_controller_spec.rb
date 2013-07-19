0def1b497afc192561ee7571a857c25b6808711a
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-07-19 13:40:20 -0300

    some helper specs

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
 
 describe AppointmentsController do
-
+  it { should respond_to(:change) }
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe SessionsController do
-
+  it { should respond_to(:new) }
+  it { should respond_to(:create) }
+  it { should respond_to(:destroy) }
 end
