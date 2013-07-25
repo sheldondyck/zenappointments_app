a8c6c84788485bbaf497c15000c6815e0a9a888f
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-07-25 09:07:53 -0300

    updated appointments schema

--- db/migrate/20130706134534_create_appointments.rb
+++ db/migrate/20130706134534_create_appointments.rb
@@ -2,15 +2,16 @@ class CreateAppointments < ActiveRecord::Migration
   def change
     create_table :appointments do |t|
       t.integer :account_id,    :null => false
-      t.foreign_key :accounts
       t.integer :user_id,       :null => false
-      t.foreign_key :users
-      t.integer :employee_id,   :null => false
-      t.foreign_key :employees
+      t.integer :employee_id,   :null => true
       t.integer :client_id,     :null => false
-      t.foreign_key :clients
       t.datetime :time,         :null => false
       t.integer :duration,      :null => false
+
+      t.foreign_key :accounts
+      t.foreign_key :users
+      t.foreign_key :employees
+      t.foreign_key :clients
 
       t.timestamps
     end
