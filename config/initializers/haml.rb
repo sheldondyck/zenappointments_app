369c774ea1e54f5c2438c88763ceb7cd5ff0c150
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-08-19 14:00:57 -0300

    added haml initializer

---
+++ config/initializers/haml.rb
@@ -0,0 +1,4 @@
+Haml::Template.options[:format] = :html5
+# defaults to true in production, false all other places
+#Haml::Template.options[:ugly] = true
+#Haml::Template.options[:remove_whitespace] = true
