9554f85e19f9842f63ce8dadb21eb246651ca557
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-08-28 09:32:19 -0300

    spec change

--- spec/controllers/appointments_controller_spec.rb
+++ spec/controllers/appointments_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe AppointmentsController do
+  it { should respond_to(:new) }
   it { should respond_to(:index) }
   it { should respond_to(:create) }
   it { should respond_to(:move) }
