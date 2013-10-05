c257312ec1cfe4df500982baaf03489a7da96b35
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-10-04 23:36:08 -0300

    added hour seperator lines in preperation for finer duration control. cleaned up week and agenda helpers

--- app/helpers/agenda_helper.rb
+++ app/helpers/agenda_helper.rb
@@ -5,8 +5,12 @@ module AgendaHelper
   end
 
   class Agenda < Struct.new(:view, :date, :employees, :callback)
+    # TODO duplicated in week_helper
     FIRST_HOUR = 0
     LAST_HOUR = 24
+    APPOINTMENT_SLOTS = 4
+    APPOINTMENT_DURATION = 60
+    SLOT_DURATION = 60 / APPOINTMENT_SLOTS
 
     delegate :content_tag, to: :view
 
@@ -19,55 +23,80 @@ module AgendaHelper
     def header
       content_tag :tr do
         content_tag(:th, '', class: 'corner').html_safe +
-        employees.map { |employee| content_tag :th, 'Employee #' + employee.to_s }.join.html_safe
+        employees.map do |employee|
+          content_tag :th, 'Employee #' + employee.to_s
+        end.join.html_safe
       end
     end
 
     def hour_rows
       hours.map do |hour|
-        content_tag :tr,
-          class: 'hour-row',
-          data: {
-            hour: hour,
-            interval: "#{hour}:00 - #{hour + 1}:00", # TODO this cal. is duplicated in card.html.
-            duration: 60 # TODO magic number duration here. should be dynamical
-          } do
-          hour_cell(hour).html_safe +
-          employees.map { |employee| employee_cell(employee, hour, 60) }.join.html_safe
+        content_tag :tr, class: 'hour-row', data: hour_row_data(hour) do
+          hour_row_cell(hour).html_safe +
+          employees.map do |employee|
+            employee_cell(employee, hour, APPOINTMENT_DURATION)
+          end.join.html_safe
         end
       end.join.html_safe
     end
 
-    def hour_cell(hour)
-      content_tag :td, '%02d:00' % hour, class: hour_classes(hour)
-    end
-
-    def employee_cell(employee, hour, duration)
-      # TODO: code smell
-      # 1. too long
-      # 2 date_pretty formatting is duplicated in controller
-      # 3. duplicate in week_agenda
-      content_tag :td, view.capture(employee, hour, &callback),
-        class: employee_classes(hour),
-        data: {
-          weekday: date.strftime('%A'), # TODO should be removed. since is duplicated in the header th
-          date: date.to_s,
-          date_pretty: date.strftime('%B %e %Y') # TODO this format is duplicated in controller
-        }
-    end
-
-    def hour_classes(hour)
+    def hour_row_class(hour)
       classes = []
       classes << 'hour'
       classes.empty? ? nil : classes.join(' ')
     end
 
-    def employee_classes(hour)
+    def hour_row_cell(hour)
+      content_tag :td, '%02d:00' % hour, class: hour_row_class(hour)
+    end
+
+    def hour_row_data(hour)
+      data = {
+        hour: hour,
+        # TODO this cal. is duplicated in card.html.
+        interval: "#{hour}:00 - #{hour + 1}:00",
+        duration: APPOINTMENT_DURATION }
+    end
+
+    def employee_cell(employee, hour, duration)
+      content_tag :td, class: employee_class(hour), data: employee_data(date) do
+        appointment_slots(date, hour)
+      end
+    end
+
+    def employee_class(hour)
       classes = []
       classes << 'edit-hour'
       # TODO: OK to remove "hour-n"?
       #classes << 'hour-' + hour.to_s
       classes.empty? ? nil : classes.join(' ')
+    end
+
+    def employee_data(date)
+      # TODO: code smell
+      # 2 date_pretty formatting is duplicated in controller
+      # 3. duplicate in week_agenda
+      data = {
+        # TODO should be removed. since is duplicated in the header th
+        weekday: date.strftime('%A'),
+        date: date.to_s,
+        # TODO this format is duplicated in controller
+        date_pretty: date.strftime('%B %e %Y') }
+    end
+
+    def appointment_slots(day, hour)
+      (0...APPOINTMENT_SLOTS).map do |slot|
+        content_tag :div,
+          view.capture(day, hour, slot, SLOT_DURATION, &callback),
+          class: appointmet_slot_class(slot)
+      end.join.html_safe
+    end
+
+    def appointmet_slot_class(slot)
+      classes = []
+      classes << "slot"
+      classes << "slot-#{slot}"
+      classes.join(" ")
     end
 
     def hours
--- app/helpers/week_helper.rb
+++ app/helpers/week_helper.rb
@@ -6,8 +6,12 @@ module WeekHelper
 
   class Week < Struct.new(:view, :date, :callback)
     START_DAY = :sunday
+    # TODO duplicated in agenda_helper
+    APPOINTMENT_SLOTS = 4
     FIRST_HOUR = 0
     LAST_HOUR = 24
+    APPOINTMENT_DURATION = 60
+    SLOT_DURATION = 60 / APPOINTMENT_SLOTS
 
     delegate :content_tag, to: :view
 
@@ -19,55 +23,48 @@ module WeekHelper
 
     def header
       content_tag :tr do
-        content_tag(:th, '', class: 'corner').html_safe +
-        weeks.map do |week|
-          week.map do |day|
-            content_tag :th, day.strftime('%A'),
-              class: 'day-col',
-              data: {
-                weekday: day.strftime('%A') # TODO this is duplicated in th col. how do we get this in onclick?
-              }
+        content_tag :th, class: 'corner' do
+          weeks.map do |week|
+            week.map do |day|
+              content_tag :th, day.strftime('%A'),
+                class: 'day-col',
+                 # TODO this is duplicated in th col. how do we get this in onclick?
+                data: { weekday: day.strftime('%A') }
+            end.join.html_safe
           end.join.html_safe
-        end.join.html_safe
+        end
       end
     end
 
     def week_rows
-      "<div class='hour-grid'>".html_safe +
-      hours.map do |hour|
-        content_tag :tr,
-          class: 'hour-row',
-          data: {
-            hour: hour,
-            interval: "#{hour}:00 - #{hour + 1}:00", # TODO this cal. is duplicated in card.html.
-            duration: 60 # TODO magic number duration here. should be dynamical
-          } do
-          hour_cell(hour, 60).html_safe +
-          weeks.map do |week|
-            week.map do |day|
-              appointment_cell(day, hour)
+      content_tag :div, class: 'hour-grid' do
+        hours.map do |hour|
+          content_tag :tr, class: 'hour-row', data: hour_row_data(hour) do
+            hour_row_cell(hour, APPOINTMENT_DURATION).html_safe +
+            weeks.map do |week|
+              week.map do |day|
+                hour_cell(day, hour)
+              end
             end.join.html_safe
-          end.join.html_safe
-        end
-      end.join.html_safe +
-      "</div>".html_safe
+          end
+        end.join.html_safe
+      end
     end
 
-    def hour_cell(hour, duration)
-      content_tag :td, '%02d:00' % hour, class: hour_classes(hour)
+    def hour_row_data(hour)
+      data = {
+        hour: hour,
+        # TODO this cal. is duplicated in card.html.
+        interval: "#{hour}:00 - #{hour + 1}:00",
+        # TODO magic number duration here. should be dynamical
+        duration: APPOINTMENT_DURATION }
     end
 
-    def appointment_cell(day, hour)
-      content_tag :td, view.capture(day, hour, &callback),
-        class: appointment_classes(hour),
-        data: {
-          weekday: day.strftime('%A'), # TODO should be removed. since is duplicated in the header th
-          date: day.to_s,
-          date_pretty: day.strftime('%B %e %Y') # TODO this format is duplicated in controller
-        }
+    def hour_row_cell(hour, duration)
+      content_tag :td, '%02d:00' % hour, class: hour_row_class(hour)
     end
 
-    def hour_classes(hour)
+    def hour_row_class(hour)
       classes = []
       classes << 'hour'
       #classes << "today" if day == Date.today
@@ -76,7 +73,13 @@ module WeekHelper
       classes.empty? ? nil : classes.join(" ")
     end
 
-    def appointment_classes(hour)
+    def hour_cell(day, hour)
+      content_tag :td, class: hour_class(hour), data: hour_data(day, hour) do
+        appointment_slots(day, hour)
+      end
+    end
+
+    def hour_class(hour)
       classes = []
       classes << 'edit-hour'
       #classes << 'hour-' + hour.to_s
@@ -84,6 +87,30 @@ module WeekHelper
       #classes << "notmonth" if day.month != date.month
       #classes << 'edit-day'
       classes.empty? ? nil : classes.join(" ")
+    end
+
+    def hour_data(day, hour)
+      data = {
+        # TODO should be removed. since is duplicated in the header th
+        weekday: day.strftime('%a'),
+        date: day.to_s,
+        # TODO this format is duplicated in controller
+        date_pretty: day.strftime('%b %e %y') }
+    end
+
+    def appointment_slots(day, hour)
+      (0...APPOINTMENT_SLOTS).map do |slot|
+        content_tag :div,
+          view.capture(day, hour, slot, SLOT_DURATION, &callback),
+          class: appointmet_slot_class(slot)
+      end.join.html_safe
+    end
+
+    def appointmet_slot_class(slot)
+      classes = []
+      classes << "slot"
+      classes << "slot-#{slot}"
+      classes.join(" ")
     end
 
     def weeks
