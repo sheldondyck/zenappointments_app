eab46cb7504b02a6813d5ba9e6ba3b7da370c763
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-10-18 10:55:49 -0300

    added comment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+// TODO: only import required files. load only needed jquery bootstrap files
 //= require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
