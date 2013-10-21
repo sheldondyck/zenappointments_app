e88d4311996ba8a142e47821624dd9322b9cce7e
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-10-21 19:15:47 -0200

    first pass at sidebar calendar

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -1,25 +1,26 @@
 # TODO agenda_help week_helper calendar_help should be combined
 module CalendarHelper
-  def calendar(date = Date.today, &block)
-    Calendar.new(self, date, block).table
+  def calendar(klass, date = Date.today, &block)
+    Calendar.new(self, date, block).table(klass)
   end
 
   class Calendar < Struct.new(:view, :date, :callback)
     HEADER = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+    HEADER_COMPACT = %w[S M T W T F S]
     # TODO: should we get this value from Account.start_of_week?
     START_DAY = :sunday
 
     delegate :content_tag, to: :view
 
-    def table
-      content_tag :table, class: "calendar" do
+    def table(klass)
+      content_tag :table, class: klass do
         header + week_rows
       end
     end
 
     def header
       content_tag :tr do
-        HEADER.map { |day| content_tag :th, day }.join.html_safe
+        HEADER_COMPACT.map { |day| content_tag :th, day }.join.html_safe
       end
     end
 
