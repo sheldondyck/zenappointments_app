c50aab91b142b01828b9ee98137748d6b5088ab8
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-10-21 23:16:44 -0200

    css clean up. better hour/slot height calculation

--- app/models/account.rb
+++ app/models/account.rb
@@ -40,7 +40,7 @@ class Account < ActiveRecord::Base
     # Valid values: 2, 3, 4, 5, 6, 8
     # Invalid values: 7
     # TODO: should we allow 3, 5?
-    4
+    6
   end
 
   def self.minutes_per_slot
