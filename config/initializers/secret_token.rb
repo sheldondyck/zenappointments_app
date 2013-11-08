0cc9e3cfe964bef2b8a6e02985e06bf1ed90ace0
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-11-08 19:10:32 -0200

    figaro heroku problem

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,5 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ZenAppointmentsApp::Application.config.secret_key_base = Figaro.env.secret_key_base
+#ZenAppointmentsApp::Application.config.secret_key_base = Figaro.env.secret_key_base
+ZenAppointmentsApp::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
