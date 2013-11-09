45ec03b0088a99ec6c1d524066f7068657904d09
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-11-09 10:01:10 -0200

    added signed cookies

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,7 +6,7 @@ module SessionsHelper
   def sign_in(user)
     #puts 'sign_in: ' + user.to_yaml
     signin_token = User.new_signin_token
-    cookies.permanent[:signin_token] = signin_token
+    cookies.permanent.signed[:signin_token] = signin_token
     user.update_attribute(:signin_token, User.encrypt(signin_token))
     self.current_user = user
   end
@@ -22,7 +22,7 @@ module SessionsHelper
   end
 
   def current_user
-    signin_token = User.encrypt(cookies[:signin_token])
+    signin_token = User.encrypt(cookies.signed[:signin_token])
     @current_user ||= User.find_by(signin_token: signin_token)
   end
 end
