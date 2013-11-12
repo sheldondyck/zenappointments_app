40f7c276d6b5d90fede9e7c1d81a4b2810a910ec
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-11-11 22:41:25 -0200

    removed unused code from js and css files 50% reduction in size

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,15 +10,63 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-// TODO: only import required files. load only needed jquery bootstrap files
 //= require jquery
 //= require jquery_ujs
-//= require jquery.ui.all
+//
+//  require jquery.ui.all
+//  require jquery.ui.accordion
+//  require jquery.ui.autocomplete
+//  require jquery.ui.button
+//  require jquery.ui.core
+//  require jquery.ui.datepicker
+//  require jquery.ui.dialog
+//= require jquery.ui.draggable
+//= require jquery.ui.droppable
+//  require jquery.ui.effect-blind
+//  require jquery.ui.effect-bounce
+//  require jquery.ui.effect-clip
+//  require jquery.ui.effect-drop
+//  require jquery.ui.effect-explode
+//  require jquery.ui.effect-fade
+//  require jquery.ui.effect-fold
+//  require jquery.ui.effect-highlight
+//  require jquery.ui.effect-pulsate
+//  require jquery.ui.effect-scale
+//  require jquery.ui.effect-shake
+//  require jquery.ui.effect-slide
+//  require jquery.ui.effect-transfer
+//  require jquery.ui.effect
+//  require jquery.ui.menu
+//  require jquery.ui.mouse
+//  require jquery.ui.position
+//  require jquery.ui.progressbar
+//= require jquery.ui.resizable
+//  require jquery.ui.selectable
+//  require jquery.ui.slider
+//  require jquery.ui.sortable
+//  require jquery.ui.spinner
+//  require jquery.ui.tabs
+//  require jquery.ui.tooltip
+//  require jquery.ui.widget
+//
+//  require bootstrap
+//  require bootstrap/affix
+//  require bootstrap/alert
+//= require bootstrap/button
+//  require bootstrap/carousel
+//  require bootstrap/collapse
+//  require bootstrap/dropdown
+//  require bootstrap/tab
+//  require bootstrap/transition
+//  require bootstrap/scrollspy
+//  require bootstrap/modal
+//  require bootstrap/tooltip
+//  require bootstrap/popover
+//
+//= require chart
 // require angular
 // require angular-resource
 // require turbolinks
-//= require bootstrap
-//= require chart
 //
 //= require_tree .
 //= require_self
