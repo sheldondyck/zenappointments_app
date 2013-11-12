2e93e4221f670b539830461c20ba2d5fa9040142
Sheldon Van Dyck <sheldon.dyck@gmail.com>
2013-11-12 10:19:14 -0200

    comment

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
-# Be sure to restart your server when you modify this file.
-
+# generated with:
+#   $ rake secret
 ZenAppointmentsApp::Application.config.session_store :cookie_store, key: '_ZenAppointmentsApp_session'
